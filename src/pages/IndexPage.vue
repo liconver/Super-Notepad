81180c3df1426dea1f4c8cc96831c8f356bbe1d3
liconver <liamc@vt.edu>
2022-07-23 11:07:18 -0400

    creates notes from editor

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -14,29 +14,94 @@
       rewind: true, pagination: false, drag: 'free', isNavigation: true, fixedWidth: '250px', gap: '2rem',
       breakpoints:
         { 1024: { width: '60w' }, 768: { width: '80vw' }, 480: { width: '100vw' } }
-    }">
-      <SplideSlide v-for="note in notes" :note="note" :key="note.date">
-        <q-card class="my-card text-black q-ma-sm" @click="modal = true"
-          style="background: radial-gradient(circle, #f2dd1a 0%, #fff800 100%)">
-          <q-card-section class="q-pb-sm overflow-hidden">
-            <div class="text-h5 ">{{ note.title }}</div>
-          </q-card-section>
-          <q-card-section class="q-pt-none overflow-hiden-y">
-            <div class="row justify center">{{ note.text }}</div>
-          </q-card-section>
-        </q-card>
-      </SplideSlide>
+    }" :has-track="false">
+      <SplideTrack>
+        <SplideSlide v-for="note in notes" :note="note" :key="note.date">
+          <q-card class="my-card text-black q-ma-sm" @click="modal2 = true"
+            style="background: radial-gradient(circle, #f2dd1a 0%, #fff800 100%)">
+            <q-card-section class="q-pb-sm overflow-hidden">
+              <div class="text-h5 ">{{ note.title }}</div>
+            </q-card-section>
+            <q-card-section class="q-pt-none overflow-hiden-y">
+              <div class="row justify center">{{ note.text }}</div>
+            </q-card-section>
+          </q-card>
+          <q-dialog v-model="modal2" full-height full-width>
+            <q-card class="column full-height bg-primary no-wrap">
+              <q-toolbar class="q-pb-none text-black " style="height: 56px">
+                <q-input v-show="addTitle" class="col-10 text-h6 q-pa-none q-ma-none" dark="false" borderless
+                  v-model="note.title" placeholder="title..." />
+                <q-space />
+                <q-btn icon="more_vert" flat round dense size="md">
+                  <q-menu anchor="bottom left" self="top right" class="bg-primary q-pa-md">
+                    <q-list dense>
+                      <q-item>
+                        <q-toggle dark="false" dense color="secondary" class="text-black" v-model="addTitle"
+                          label="Add Title" />
+                      </q-item>
+                      <q-item>
+                        <q-toggle dark="false" dense color="secondary" class="text-black" v-model="showToolbar"
+                          label="Show Toolbar" />
+                      </q-item>
+                      <q-item>
+                        <q-item-section>New tab</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-btn>
+              </q-toolbar>
+              <q-editor v-if="showToolbar" dark="false" placeholder="..." toolbar-push toolbar-text-color="black"
+                toolbar-toggle-color="secondary" toolbarColor="primary" toolbarBg="primary" flat height="60vh"
+                class="bg-primary text-black" v-model="note.text" :toolbar="[
+                  ['unordered', 'ordered', 'outdent', 'indent',
+                    {
+                      icon: $q.iconSet.editor.fontSize,
+                      fixedIcon: true,
+                      list: 'no-icons',
+                      options: [
+                        'size-1',
+                        'size-2',
+                        'size-3',
+                        'size-4',
+                        'size-5',
+                        'size-6',
+                        'size-7'
+                      ]
+                    },
+                    {
+                      label: $q.lang.editor.align,
+                      icon: $q.iconSet.editor.align,
+                      fixedLabel: true,
+                      list: 'only-icons',
+                      options: ['left', 'center', 'right', 'justify']
+                    },
+                    'quote', 'link', 'bold', 'italic', 'underline', 'strike',
+                    'undo', 'redo', 'removeFormat', 'viewsource', 'print'], //TODO: add attach icon
+                ]" />
+              <q-editor v-else placeholder="..." height="70vh" flat class="bg-primary  text-black" v-model="note.text"
+                min-height="5rem" :toolbar="[]" />
+              <q-card-section class="col q-pt-none">
+              </q-card-section>
+              <q-card-actions align="right" class="bg-primary text-black">
+                <q-btn flat icon="close" round v-close-popup />
+              </q-card-actions>
+            </q-card>
+          </q-dialog>
+        </SplideSlide>
+      </SplideTrack>
+      <div v-if="multiple" class="splide__arrows" />
     </Splide>
 
+
     <div>
-      <q-btn round size="lg" class="fixed-bottom-right" icon="edit" text-color="primary" @click="modal = true">
+      <q-btn round size="lg" class="fixed-bottom-right" icon="edit" text-color="primary" @click="modal1 = true">
         <q-tooltip transition-show="rotate" transition-hide="rotate" anchor="top left" self="bottom middle"
           class="bg-primary text-black text-body2" :offset="[40, 14]">
           Create a new note
         </q-tooltip>
       </q-btn>
 
-      <q-dialog v-model="modal" full-height full-width>
+      <q-dialog v-model="modal1" full-height full-width>
         <q-card class="column full-height bg-primary no-wrap">
           <q-toolbar class="q-pb-none text-black " style="height: 56px">
             <q-input v-show="addTitle" class="col-10 text-h6 q-pa-none q-ma-none" dark="false" borderless
@@ -88,15 +153,12 @@
                   options: ['left', 'center', 'right', 'justify']
                 },
                 'quote', 'link', 'bold', 'italic', 'underline', 'strike',
-                'undo', 'redo', 'removeFormat', 'viewsource', 'print'],
+                'undo', 'redo', 'removeFormat', 'viewsource', 'print'], //TODO: add attach icon
             ]" />
           <q-editor v-else placeholder="..." height="70vh" flat class="bg-primary  text-black" v-model="newNote.text"
             min-height="5rem" :toolbar="[]" />
-
-
           <q-card-section class="col q-pt-none">
           </q-card-section>
-
           <q-card-actions align="right" class="bg-primary text-black">
             <q-btn flat icon="close" round v-close-popup />
           </q-card-actions>
@@ -107,37 +169,66 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import { ref, watch } from 'vue'
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/vue-splide';
 import '@splidejs/vue-splide/css/core';
+//import SplideTrack from '../../node_modules/%40splidejs/vue-splide/src/js/components/SplideTrack/SplideTrack.vue';
 
-const modal = ref(false)
+const modal1 = ref(false)
+const modal2 = ref(false)
 const showToolbar = ref(false)
 const addTitle = ref(false)
+const multiple = ref(false)
 
 const newNote = ref({
   text: '',
   title: '',
-  date: new Date()
+  date: new Date(),
 })
 
 const notes = ref([
-  {
-    text: 'Coud this work for n0tes?',
-    title: 'Hello Quasar',
-    date: new Date()
-  },
-  {
-    text: 'This is a note',
-    title: 'Note 1',
-    date: new Date()
-  },
-  {
-    text: 'This is another note',
-    title: 'Note 2',
-    date: new Date()
-  }
+  // {
+  //   text: 'Coud this work for n0tes?',
+  //   title: 'Hello Quasar',
+  //   date: new Date()
+  // },
+  // {
+  //   text: 'This is a note',
+  //   title: 'Note 1',
+  //   date: new Date()
+  // },
+  // {
+  //   text: 'This is another note',
+  //   title: 'Note 2',
+  //   date: new Date()
+  // }
 ])
+
+watch(
+  () => modal1.value,
+  () => {
+    console.log('watch1 ran');
+    if (newNote.value.text.length > 0) {
+      notes.value.push({
+        text: newNote.value.text,
+        title: newNote.value.title,
+        date: new Date()
+      })
+      newNote.value.text = ''
+      newNote.value.title = ''
+    }
+  }
+)
+watch(
+  () => notes.value.length,
+  () => {
+    console.log('watch2 ran');
+    console.log(notes.value.length);
+    if (notes.value.length > 1) {
+      multiple.value = true
+    }
+  }
+)
 
 </script>
 
